7c0a6741623f4e0b9b9ac95939ca5bfe2f18e1c2
Frank Schumann <frank.schumann.76@gmail.com>
2023-01-24 17:30:45 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//#include <hello.h>
+
+#include "config.h"
+
+int main()
+{
+    std::cout << "consumer" << std::endl;
+
+    std::cout << "version: " << APPLICATION_VERSION << std::endl;
+}
